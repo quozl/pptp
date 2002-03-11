d0d14f33ad5a461d7c14026cc90838a88d3c9aa0
quozl <quozl>
2002-03-11 01:50:47 +0000

    add include of config.h

--- version.c
+++ version.c
@@ -1,7 +1,8 @@
 /* version.c ..... keep track of package version number. 
  *                 C. Scott Ananian <cananian@alumni.princeton.edu>
  *
- * $Id: version.c,v 1.1 2000/12/23 08:19:51 scott Exp $
+ * $Id: version.c,v 1.2 2002/03/11 01:50:47 quozl Exp $
  */
 
+#include "config.h"
 const char * version = "pptp-linux version " PPTP_LINUX_VERSION;
